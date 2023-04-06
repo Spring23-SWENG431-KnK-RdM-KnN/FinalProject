34f411d6c3e4b756ddcb2e4e9d42b9d286c63bd8
Keian Kaserman <kaserman22@gmail.com>
2023-04-06 00:44:54 -0400

    Triangle problem solution

---
+++ Triangle/src/TriangleDeterminer.java
@@ -0,0 +1,77 @@
+/**
+ * Code adapted with little change from
+ * pseudo code Module 2 PartII.2 Unit Testing Illustration_1
+ * The Triangle Program on canvas.
+ */
+public class TriangleDeterminer {
+
+    private final String nonTri = "NotATriangle";
+    private final String scalene = "Scalene";
+    private final String equilat = "Equilateral";
+    private final String isosc = "Isosceles";
+
+    /**
+     * Returns the match value based on 3 provided triangle
+     * side lengths
+     * @param a triangle side
+     * @param b triangle side
+     * @param c triangle side
+     * @return match value used to determine triangle type
+     */
+    public int getMatchValue(int a, int b, int c) {
+        int match = 0;
+        if (a == b) {
+            match += 1;
+        }
+        if (a == c) {
+            match += 1;
+        }
+        if (b == c) {
+            match += 1;
+        }
+
+        return match;
+    }
+
+    /**
+     * Returns string title of triangle type
+     * @param match number of matching sides
+     * @param a triangle side
+     * @param b triangle side
+     * @param c triangle side
+     * @return string name of triangle type
+     */
+    public String getTriangleType(int match, int a, int b, int c) {
+        if (match == 0) {
+            if (a + b <= c) {
+                return nonTri;
+            } else if (b + c <= a) {
+                return nonTri;
+            } else if (a + c <= b) {
+                return nonTri;
+            } else {
+                return scalene;
+            }
+        } else if (match == 1) {
+            if (a + c <= b) {
+                return nonTri;
+            } else {
+                return isosc;
+            }
+        } else if (match == 2) {
+            if (a + c <= b) {
+                return nonTri;
+            } else {
+                return isosc;
+            }
+        } else if (match == 3) {
+            if (b + c <= a) {
+                return nonTri;
+            } else {
+                return isosc;
+            }
+        } else {
+            return equilat;
+        }
+    }
+}
